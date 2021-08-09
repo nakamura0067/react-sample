3dbacc31b23d29736e31d8932fcaa38245efd33f
nakamura <nakamura0067@gmail.com>
2021-08-09 18:32:46 +0900

    todoの修正

--- todo-app/src/Todo.js
+++ todo-app/src/Todo.js
@@ -5,31 +5,32 @@ import useTodo from './hooks/useTodo';
 import TodoForm from './components/TodoForm';
 import TodoFilter from './components/TodoFilter';
 
-//Todoのstateを取得
-const {todos, toggleTodo, deleteTodo, addTodo} = useTodo();
-
-//選択中の絞り込みのstateとfilterを更新する関数
-const [filter, setFilter] = useState('all');
-
-//絞り込みのラジオボタンのイベントハンドラ
-const handleFilter = event => {
-  setFilter(event.target.value);
-};
-
-// 絞り込みを適用したTodo
-const filteredTodos = useMemo(() => {
-  switch(filter) {
-    case 'active':
-      return todos.filter(todo => !todo.completed);
-    case 'completed':
-      return todos.filter(todo => todo.completed);
-    case 'all':
-    default:
-      return todos;
-  }
-}, [todos, filter]);
-
 export default function Todo() {
+
+  //Todoのstateを取得
+  const {todos, toggleTodo, deleteTodo, addTodo} = useTodo();
+
+  //選択中の絞り込みのstateとfilterを更新する関数
+  const [filter, setFilter] = useState('all');
+
+  //絞り込みのラジオボタンのイベントハンドラ
+  const handleFilter = event => {
+    setFilter(event.target.value);
+  };
+
+  // 絞り込みを適用したTodo
+  const filteredTodos = useMemo(() => {
+    switch(filter) {
+      case 'active':
+        return todos.filter(todo => !todo.completed);
+      case 'completed':
+        return todos.filter(todo => todo.completed);
+      case 'all':
+      default:
+        return todos;
+    }
+  }, [todos, filter]);
+
   return (
     <div>
       <h1>Todo List</h1>
