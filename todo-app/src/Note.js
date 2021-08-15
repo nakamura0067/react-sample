28e81045363a65a85d0e18963f4aacf2c5e0db8e
nakamura <nakamura0067@gmail.com>
2021-08-15 17:26:32 +0900

    noteのui修正

--- todo-app/src/Note.js
+++ todo-app/src/Note.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-class Note extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>Note</p>
-            </div>
-        );
-    }
+export default function Note(){
+  return (
+    <form>
+      <div style={{padding:"10px","max-height":"10vh"}}>
+      <TextField
+        id="date"
+        label="Date"
+        type="date"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        style={{
+          width:"30vh"
+        }}
+      />
+       <TextField id="standard-basic" label="Title" style={{width:"60vh"}}/>
+      </div>
+      <div style={{width:"100vh",height:"50vh"}}>
+        <TextField
+          id="outlined-multiline-static"
+          label="Text"
+          multiline
+          rows={15}
+          variant="outlined"
+          style={{width:"90vh",height:"50vh",left:"10px"}}
+        />
+      </div>
+      <div style={{padding:"10px",height:"5vh"}}>
+      <Button variant="contained" color="primary">
+        Save
+      </Button>
+      </div>
+    </form>
+  );
 }
-
-export default Note;
